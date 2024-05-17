b8b5330f0ec3400d6523d05ac0759a8e692e796c
DownHouseTM <125512410+DownHouseTM@users.noreply.github.com>
2024-05-18 01:34:29 +0330

    Update rand.js

--- rand.js
+++ rand.js
@@ -1,15 +1,19 @@
-const getRandomNumbers = () => {
+export const getRandomNumbers = () => {
   const rand_a = Math.random();
   const rand_b = Math.random();
   return {rand_a, rand_b}
 }
 
+const gerRandomByOrder = () => {
+  const {rand_a, rand_b} = getRandomNumbers()
 
-const ETHER = 1_000_000_000_000_000_000n
+  const bigger = Math.max(rand_a, rand_b);
+  const smaller = Math.min(rand_a, rand_b);
+  return {bigger, smaller};
+}
 
-const {rand_a, rand_b} = getRandomNumbers()
 
-const bigger = Math.max(rand_a, rand_b);
-const smaller = Math.min(rand_a, rand_b);
+
+
 
 
